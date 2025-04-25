6a53c58461c85d99043942535314605ff33dbdb7
StepanHovsepyan7 <stpnhovsepyan@gmail.com>
2025-04-25 17:48:26 +0400

    upcoming Films section

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 const API_KEY = "api_key=9b702a6b89b0278738dab62417267c49";
 let root = document.getElementById("root");
 let popularParent = document.getElementsByClassName("popularParent")[0];
+let upcomingParent = document.getElementsByClassName("upcomingParent")[0];
 let searchInp = document.getElementsByClassName("searchInp")[0];
 let title = document.getElementById("title");
+let title2 = document.getElementById("title2");
 let form = document.getElementById("form");
 let genresParent = document.getElementById("genresParent");
-let sliderParent = document.getElementById("slider");
+let upcomingmovies = document.getElementById("upcoming-Movies");
 let awards = document.getElementById("awards");
 let awardsHtml = awards.innerHTML;
 
@@ -59,6 +61,9 @@ function genres() {
 					title.innerHTML = `${e.name}`;
 					awards.innerHTML = "";
 					popularParent.innerHTML = "";
+					upcomingParent.innerHTML = "";
+					title2.innerHTML = "";
+					upcomingmovies.style.margin = "0px";
 
 					fetch(
 						`https://api.themoviedb.org/3/discover/movie?${API_KEY}&with_genres=${e.id}`
@@ -100,12 +105,15 @@ function search() {
 		timer = setTimeout(() => {
 			if (e.target.value.trim() !== "") {
 				title.innerHTML = "Results";
+				title2.innerHTML = "";
 				fetch(
 					`https://api.themoviedb.org/3/search/movie?${API_KEY}&query=${e.target.value.trim()}`
 				)
 					.then((data) => data.json())
 					.then((data) => {
 						popularParent.innerHTML = "";
+						upcomingParent.innerHTML = "";
+						upcomingmovies.style.margin = "0";
 						awards.innerHTML = "";
 
 						data.results.forEach((e) => {
@@ -124,12 +132,39 @@ function search() {
 			} else {
 				title.innerHTML = "Popular Films";
 				awards.innerHTML = awardsHtml;
+				upcomingmovies.style.margin = "150px";
 				loadPopular();
+				upcomingMovies();
 			}
 		}, 1000);
 	});
 }
 
 search();
+
+function upcomingMovies() {
+	upcomingParent.innerHTML = "";
+	upcomingmovies.style.margin = "150px";
+	title2.innerHTML = "Upcoming Movies";
+	fetch(`https://api.themoviedb.org/3/movie/upcoming?` + API_KEY)
+		.then((res) => res.json())
+		.then((data) => {
+			data.results.forEach((e) => {
+				upcomingParent.innerHTML += `
+			          <div class="parentCard"  data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
+                <img src=${img_url + e.poster_path}/>
+                <div class = "contentCard">
+                    <span class = "spans spansTitle">${e.title}</span>
+                    <span class = "spans">Rating: ${e.vote_average}</span>
+                    <span class = "spans">${e.release_date}</span>
+                </div>
+            </div>
+			
+			`;
+			});
+		});
+}
+
+upcomingMovies();
 
 AOS.init();
